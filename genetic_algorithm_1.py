75b9b353c62bf4427bb1cdb4f0a797b77daebb3c
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-03-31 13:52:06 -0500

    added probability to mutate

--- genetic_algorithm_1.py
+++ genetic_algorithm_1.py
@@ -10,8 +10,12 @@ iterations = 30
 cross_prob = 0.9
 cross_point = 3
 mutation_prob = 0.05
+# f(x) = x^2
 
 data = []
+
+def fitness_function_1(x):
+	return x*x
 
 def generate_population(n_population, cromosome_size):
 	population =  []
@@ -26,12 +30,11 @@ def eval_population():
 	fitness = []
 	for i in data:
 		b = BitArray(i[0])
-		i.append([b.uint])
+		i.append([fitness_function_1(b.uint)]) # x^2
 	
 
 def get_parent(roulette_range):
 	selected = random.uniform(0,100)
-
 	for i in roulette_range:
 		if( i[0] <= selected and selected <=i[1] ):
 			index = roulette_range.index(i)
@@ -47,13 +50,11 @@ def roulette_selection():
 	print("Selecting by Roulette:")
 	print('\n'.join(' '.join(''.join( map(str, j)) for j in i) \
 						 + ' - '+str(ms)  for i, ms in zip(data, percents) ))
-
 	roulette_range = []
 	tmp = 0
 	for i in percents:
 		roulette_range.append([tmp, i+tmp])
 		tmp += i
-
 	while(True):
 		mother_index = get_parent(roulette_range)
 		father_index = get_parent(roulette_range)
@@ -61,7 +62,6 @@ def roulette_selection():
 			selected.append(mother_index)
 			selected.append(father_index)
 			break
-
 	print("Mother: " + str(mother_index))
 	print("Father: " + str(father_index))
 
@@ -97,10 +97,9 @@ def selecting_next_population():
 	tmp = sorted(tmp, key=itemgetter(1), reverse=True)
 	ms = []
 	for i in range(n_population):
-		print(data[tmp[i][0]])
 		ms.append(data[tmp[i][0]])
 	data = ms
-1
+
 def genetic_algorithm():
 	global data
 	generate_population(n_population, cromosome_size)
@@ -114,18 +113,25 @@ def genetic_algorithm():
 		while(True):
 			if( len(data) + len(tmp) >= n_population*2):
 				break
-			selected = roulette_selection()
-			offspring = one_point_crossover(cross_point, selected[0], selected[1])
-			for i in offspring:
-				b = BitArray(i)
-				tmp.append([i,[b.uint]])
+			if( random.uniform(0,100) <= cross_prob*100 ): # Crossove Prob
+				selected = roulette_selection()
+				offspring = one_point_crossover(cross_point, selected[0], selected[1])
+				for son in offspring:
+					if( random.uniform(0,100) <= mutation_prob*100 ): # Mutation Prob
+						print("Mutation")
+						tmp_son = son
+						son[random.randint(0,len(son)-1)] = 1
+						print(tmp_son)
+						print(son)
+					b = BitArray(son)
+					tmp.append([son,[fitness_function_1(b.uint)]])
+
 		for i in tmp:
 			data.append(i)
 		selecting_next_population()
 	print("Evaluating individuals:")
 	print('\n'.join(' '.join(''.join(map(str, j))for j in i)for i in data ))
 
-			
-	# selecting_next_population()
+
 
 genetic_algorithm()
