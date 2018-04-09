32508fdb305c83706ab934be41bc2b6128c4ccd2
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-04-09 18:58:26 -0500

    more crossover algorithms

--- ga_tsp.py
+++ ga_tsp.py
@@ -4,8 +4,6 @@ import random
 from operator import itemgetter
 import math
 
-
-
 cities = [0, 1, 2, 3, 4]
 roads = dict()
 
@@ -40,13 +38,16 @@ roads[4,3] = 4
 # 		  [3,0,1], [3,1,2], [3,2,2], [3,4,4], \
 # 		  [4,0,4], [4,1,3], [4,2,2], [4,3,4] ]
 
-n_population = 100
+n_population = 5
 cromosome_size = len(cities)
 
-iterations = 100
+iterations = 3
 cross_prob = 0.9
 mutation_prob = 0.05
 k_adversaries = 3
+
+pmx_point_1 = 1
+pmx_point_2 = 3
 
 data = []
 
@@ -58,10 +59,7 @@ def OBX_crossover(mother_index, father_index):
 	father_cromosome = data[father_index][0]
 
 	son_1, son_2 = [], []
-
 	mask = np.random.randint(2, size=cromosome_size)
-
-
 
 	for i, j in zip(range(cromosome_size), mask):
 		if(j == 1):
@@ -92,8 +90,97 @@ def OBX_crossover(mother_index, father_index):
 	offpsring.append(son_2)
 	return offpsring
 
+def PBX_crossover(mother_index, father_index):
+	offpsring = []
 
-	return 
+	mother_cromosome = data[mother_index][0]
+	father_cromosome = data[father_index][0]
+
+	son_1, son_2 = [], []
+
+	mask = np.random.randint(2, size=cromosome_size)
+
+	for i, j in zip(range(cromosome_size), mask):
+	
+		if(j == 1):
+			son_1.append(father_cromosome[i])
+			son_2.append(mother_cromosome[i])
+		else:
+			son_1.append(-1)
+			son_2.append(-1)
+
+	print(son_1)
+	print(son_2)
+
+	for i in mother_cromosome:
+		if (i not in son_1):
+			tmp = son_1.index(-1)
+			son_1[tmp] = i
+
+	for i in father_cromosome:
+		if (i not in son_2):
+			tmp = son_2.index(-1)
+			son_2[tmp] = i
+
+
+	print(*son_1, sep='')
+	print(*son_2, sep='')
+
+	offpsring.append(son_1)
+	offpsring.append(son_2)
+	return offpsring
+
+def PMX_crossover(mother_index, father_index):
+	offpsring = []
+
+	mother_cromosome = data[mother_index][0]
+	father_cromosome = data[father_index][0]
+
+	son_1, son_2 = mother_cromosome, father_cromosome
+
+	print(*son_1, sep='')
+	print(*son_2, sep='')
+
+	for i in range(pmx_point_1, pmx_point_2+1):
+		tmp_1 = np.where(son_2 == son_1[i])
+		tmp_2 = np.where(son_1 == son_2[i])
+
+		tmp_1 = tmp_1[0]
+		tmp_2 = tmp_2[0]
+
+		son_1[i], son_2[i] = son_2[i], son_1[i]
+		son_2[tmp_1], son_1[tmp_2] = son_1[i], son_2[i]
+
+
+		print(*son_1, sep='')
+		print(*son_2, sep='')
+
+		print()
+
+
+	offpsring.append(son_1)
+	offpsring.append(son_2)
+	return offpsring
+
+def CX_crossover(mother_index, father_index):
+	offpsring = []
+
+	mother_cromosome = data[mother_index][0]
+	father_cromosome = data[father_index][0]
+
+	son_1, son_2 = [], []
+
+	print(*son_1, sep='')
+	print(*son_2, sep='')
+	print()
+
+	for i in range(len(cromosome_size)):
+		son_1.
+
+
+
+
+
 
 
 def function_fitness(x):
@@ -195,10 +282,16 @@ def genetic_algorithm():
 				break
 			if( random.uniform(0,100) <= cross_prob*100 ): # Crossove Prob
 				selected = tournament_selection(k_adversaries)
-				offspring = OBX_crossover(selected[0], selected[1])
+				offspring = PBX_crossover(selected[0], selected[1])
 				for son in offspring:
-					# if( random.uniform(0,100) <= mutation_prob*100 ): # Mutation Prob
-					# 	print("Mutation")
+					if( random.uniform(0,100) <= mutation_prob*100 ): # Mutation Prob
+					 	print("Mutation")
+					 	rand_1 = random.randint(0,cromosome_size-1)
+					 	rand_2 = random.randint(0,cromosome_size-1)
+					 	print(son)
+					 	son[rand_1], son[rand_2] = son[rand_2], son[son_1]
+					 	print(son)
+					#   mutation for 0 and 1
 					# 	tmp_son = son
 					# 	son[random.randint(0,len(son)-1)] = 1
 					# 	print(tmp_son)
