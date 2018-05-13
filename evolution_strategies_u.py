24447b85bcb48a80a138f408dd695017ac941a90
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-05-13 17:43:17 -0500

    individuals are tuples

--- evolution_strategies_u.py
+++ evolution_strategies_u.py
@@ -1,22 +1,26 @@
 import math
 import random
+import numpy as np
 
 x_lower_limit = -10.
 x_upper_limit = 10.
 deviation = .3
 individual_size = 2
 population_size = 5
-data = []
+n_adversaries = 3
+data = list()
 
 def generate_individual( size, x_lower_limit, x_upper_limit ):
-	return [ random.uniform(x_lower_limit, x_upper_limit) \
-									for i in range(size)]
+	return {"fitness": np.inf, "xs":[ random.uniform(x_lower_limit, x_upper_limit) \
+									for i in range(size)], "dev": deviation}
 
 def generate_population( population_size ):
-	for i in range( population_size ):
-		tmp = generate_individual(individual_size, x_lower_limit, x_upper_limit)
-		tmp.append( deviation )
-		data.append( tmp )
+	for x in range( population_size ):
+		data.append( generate_individual( individual_size, x_lower_limit, \
+															x_upper_limit) )
+
+# def tournament_selection( n_adversaries ):
+
 
 def fitness_function( x_1, x_2):
 	return -cos(x_1)*cos(x_2)*math.exp(-(x_1-math.pi)**2 - \
