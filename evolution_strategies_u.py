f0d07d4856c4412feb6905e9e731a840130af36f
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-05-13 20:19:56 -0500

    ep_u_1 working

--- evolution_strategies_u.py
+++ evolution_strategies_u.py
@@ -6,8 +6,15 @@ x_lower_limit = -10.
 x_upper_limit = 10.
 deviation = .3
 individual_size = 2
-population_size = 5
+population_size = 50
 n_adversaries = 3
+n_iterations = 2000
+
+infinite = -1.
+lim_inf = -4
+lim_sup = 4
+delta = 0.01
+
 data = list()
 
 def generate_individual( size, x_lower_limit, x_upper_limit ):
@@ -28,12 +35,13 @@ def tournament_selection( n_adversaries ):
 	return min(tmp, key=lambda item: item["fitness"])
 
 def crossover( parent_1, parent_2):
-	xs = [ .5*( parent_1["xs"][i] + parent_2["xs"][i] ) \
-						for i in range(len(parent_1["xs"])) ]
-	devs = [ math.sqrt( parent_1["dev"][i] + parent_2["dev"][i] ) \
-						for i in range(len(parent_1["dev"])) ]
-	fitness = fitness_function( xs[0], xs[1] )
-	return {"xs": xs, "devs": devs, "fitness": fitness}
+
+	xs = list([ .5*( parent_1["xs"][i] + parent_2["xs"][i] ) \
+						for i in range(len(parent_1["xs"])) ])
+	devs = list([ math.sqrt( parent_1["dev"][i] + parent_2["dev"][i] ) \
+						for i in range(len(parent_1["dev"])) ])
+	fitn = fitness_function( xs[0], xs[1] )
+	return {"xs": xs, "dev": devs, "fitness": fitn}
 	
 def fitness_function( x_1, x_2):
 	return -math.cos(x_1)*math.cos(x_2)*math.exp(-(x_1-math.pi)**2 - \
@@ -45,18 +53,65 @@ def valid_individual( individual ):
 		return True
 	return False
 
+def std_deviation(x, dev):
+	return (math.exp(-0.5 * (x / dev) ** 2)) / \
+		   (dev * math.sqrt(2 * math.pi))
+
+
+def integral(lim_inf, lim_sup, dev, delta, rnd):
+	area = 0.
+	aux_sum = std_deviation(lim_inf, dev)
+	aux = std_deviation(lim_inf, dev)
+
+	lin_space = np.arange(lim_inf + delta, lim_sup, delta)
+	for i in lin_space:
+		aux_sum = std_deviation(i, dev)
+		area += (aux + aux_sum)
+		if (area * (delta / 2.) ) > rnd:
+			return i
+		aux = aux_sum
+	return -1 * infinite
 
 def mutation( individual ):
-
+	for i in range( len(individual["xs"]) ):
+		individual["dev"][i] *= math.exp( integral(lim_inf, lim_sup, \
+								individual["dev"][i], delta, random.random() ))
+		individual["xs"][i] += integral(lim_inf, lim_sup, \
+								individual["dev"][i], delta, random.random() )
+	individual["fitness"] = fitness_function( individual["xs"][0], \
+													individual["xs"][1])
 
 def ep_u_1():
+	global data
+	iteration = 0
 	generate_population( population_size )
 	evaluate_population()
+
+	while( iteration < n_iterations ):
+		print("iteration #", iteration)
+
+		while( True ):
+			new_individual = crossover( tournament_selection( n_adversaries ), \
+										tournament_selection( n_adversaries ) )
+			# print("new individual\t ", new_individual)
+			mutation(new_individual)
+			# print("mutate\t", new_individual)
+			if( valid_individual(new_individual) ):
+				break
+
+		data.append( new_individual )
+		data = sorted( data, key=lambda x: x["fitness"] )
+		del data[ population_size ]
+
+		iteration+=1
+
+		for i in range( population_size ):
+			print(data[i]["fitness"])
 	print(data)
-	tournament_selection( n_adversaries )
+
 	return True
 
 
 if __name__ == "__main__":
-	ep_u_1()
-	# print( fitness_function( math.pi, math.pi) )
+	# ep_u_1()
+	# print( fitness_function( 4.1180866324905363, 2.9631102046257594) )
