51a7fb0d84de9e1ef1bbb26910b69646f21ddd9b
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-04 19:44:01 -0500

    showing swarm

--- PSO/pso.py
+++ PSO/pso.py
@@ -25,15 +25,31 @@ def function_fitness( x, y ):
 	return x**2 + y**2
 
 def create_particle():
-	x = random.uniform(min_x1, max_x1)
-	y = random.uniform(min_x2, max_x2)
+	x = random.uniform(min_x, max_x)
+	y = random.uniform(min_y, max_y)
 	v_x = random.uniform(-1, 1)
 	v_y = random.uniform(-1, 1)
 	p_best = np.inf
 	return { "pos": [x, y], "vel": [v_x, v_y], "p_best": [-1,-1,p_best] }
 
+def create_swarm():
+	for i in range( n_particles ):
+		data.append( create_particle() )
+
+def print_swarm():
+	for part in data:
+		print( "x1 = "+str(part["pos"][0])+
+			   " x2 = "+str(part["pos"][1])+
+			   " v1 = "+str(part["vel"][0])+
+			   " v2 = "+str(part["vel"][1]) )
+
 def pso():
 	print("Starting PSO")
+	create_swarm()
+	print_swarm()
+
+	for i in range( n_iterations ):
+		print("Iteration: ", i )
 
 
 if __name__=="__main__":
