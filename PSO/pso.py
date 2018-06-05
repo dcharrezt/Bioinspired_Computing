c68e09ab0388cba92d786d8643eabc011f850bca
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-04 19:31:21 -0500

    starting particle swarm optimization

---
+++ PSO/pso.py
@@ -0,0 +1,29 @@
+import numpy as np
+import random
+
+# Parameters 
+
+min_v = -1
+max_v = 1
+
+min_x1 = -5
+max_x1 = 5
+
+min_x2 = -5
+max_x2 = 5
+
+n_iterations = 3
+n_particles = 2
+
+phi_1 = 2.0
+phi_2 = 2.0
+
+data = []
+best_global = []
+
+def pso():
+	print("Starting PSO")
+
+
+if __name__=="__main__":
+	pso()
