6ca04a9911c283ffbd68a6df2619ef0605167825
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-04 20:01:36 -0500

    evaluating swarm

--- PSO/pso.py
+++ PSO/pso.py
@@ -19,7 +19,7 @@ phi_1 = 2.0
 phi_2 = 2.0
 
 data = []
-best_global = []
+best_global = { "x": -1, "y": -1, "fitness": np.inf }
 
 def function_fitness( x, y ):
 	return x**2 + y**2
@@ -29,8 +29,10 @@ def create_particle():
 	y = random.uniform(min_y, max_y)
 	v_x = random.uniform(-1, 1)
 	v_y = random.uniform(-1, 1)
+	fitness = np.inf
 	p_best = np.inf
-	return { "pos": [x, y], "vel": [v_x, v_y], "p_best": [-1,-1,p_best] }
+	return { "pos": [x, y], "vel": [v_x, v_y], "fitness": fitness,\
+						"p_best": {"x":-1,"y":-1,"fitness": p_best } } 
 
 def create_swarm():
 	for i in range( n_particles ):
@@ -43,13 +45,41 @@ def print_swarm():
 			   " v1 = "+str(part["vel"][0])+
 			   " v2 = "+str(part["vel"][1]) )
 
+# def updating_swarm():
+# 	for part in data:
+# 		for i in range( n_particles ):
+# 			w = random.random()
+# 			rand_1 = random.random()
+# 			rand_2 = random.random()
+
+def evaluating_swarm():
+	print("****** Fitness")
+	for part, i in zip(data, range(n_particles)):
+		part["fitness"] = function_fitness( part["pos"][0], part["pos"][1] )
+		if part["p_best"]["fitness"] > part["fitness"]:
+			part["p_best"]["x"] = part["pos"][0] 
+			part["p_best"]["y"] = part["pos"][1] 
+			part["p_best"]["fitness"] = part["fitness"]
+		if best_global["fitness"] > part["fitness"]:
+			best_global["x"] = part["pos"][0]
+			best_global["y"] = part["pos"][1]
+			best_global["fitness"] = part["fitness"]
+		print(str(i)+" ) "+str(part["fitness"]))
+
 def pso():
-	print("Starting PSO")
+	print("****** Starting PSO")
 	create_swarm()
 	print_swarm()
+	evaluating_swarm()
 
 	for i in range( n_iterations ):
 		print("Iteration: ", i )
+		print("Best particle so far: ")
+		print("x = "+str(best_global["x"])+" y = "+str(best_global["y"])+\
+					" fitness" +str(best_global["fitness"]))
+		print("Next swarm ")
+		updating_swarm()
+		evaluating_swarm()
 
 
 if __name__=="__main__":
