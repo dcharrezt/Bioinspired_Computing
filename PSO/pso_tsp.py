82dbb97bcfcba3c54cad5025c6b9e2557a1164f4
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-18 15:43:30 -0500

    particle swarm optimization for tsp

--- PSO/pso_tsp.py
+++ PSO/pso_tsp.py
@@ -19,19 +19,69 @@ phi_2 = 1.0
 
 data = []
 cities = [ 'A', 'B', 'C', 'D', 'E' ]
+best_global = {'path': [], 'fitness': np.inf }
 
 def create_particle():
-	return np.random.permutation( n_cities )
+	path = np.random.permutation( n_cities )
+	return {'path': path, 'fitness':np.inf, 'best_local_path':[], 
+						'b_local_fitness': np.inf, 'vel':[] }
 
-def function_fitness( path );
+def create_swarm():
+	for i in range( n_particles ):
+		data.append( create_particle() )
+
+def function_fitness( path ):
 	cost = 0.
 	for i in range( n_cities-1 ):
 		cost += distances[ path[i] ][ path[i+1] ]
 	return cost
+
+def evaluating_swarm():
+	for i in range( n_particles ):
+		data[i]['fitness'] = function_fitness( data[i]['path'] )
+		if data[i]['fitness'] < best_global['fitness']:
+			best_global['fitness'] = data[i]['fitness']
+			best_global['path'] = list(data[i]['path'])
+		if data[i]['fitness'] < data[i]['b_local_fitness']:
+			data[i]['b_local_fitness'] = data[i]['fitness']
+			data[i]['best_local_path'] = data[i]['path']
+
+def substract_permutations( path_1, path_2 ):
+	SS = []
+	tmp_1 = list( path_1 )
+	tmp_2 = list( path_2 )
+	for i in range( n_cities ):
+		if tmp_1[i] != tmp_2[i]:
+			index = tmp_2.index( tmp_1[i] )
+			SS.append([i , index])
+			tmp = tmp_2[i]
+			tmp_2[i] = tmp_2[index]
+			tmp_2[index] = tmp 
+	return SS
 	
+def update_particle( particle ):
+	for i in particle['vel']:
+		tmp = particle['path'][i[0]]
+		particle['path'][i[0]] = particle['path'][i[1]]
+		particle['path'][i[1]] = tmp
+
 def pso():
 	print("****** Starting PSO")
-
+	create_swarm()
+	for i in range( n_iterations ):
+		print( "Iteration " + str(i+1) + " ******************* ")
+		evaluating_swarm()
+		for i in data:
+			print("Current: ", i['path'])
+			print("Fitness: ", i['fitness'] )
+			print("Best local", i['best_local_path'] )
+			print("Fitness Best local", i['b_local_fitness'])
+			print("Velocity: ", i['vel'] , end='\n\n' )
+			a = substract_permutations( i['path'], best_global['path'])
+			b = substract_permutations( i['path'], i['best_local_path'])
+			i['vel'] = a + b
+			update_particle( i )
 
 if __name__=="__main__":
 	pso()
+	# print ( substract_permutations( [0,2, 4, 1, 3], [0, 1, 4, 3, 2]  ) ) 
