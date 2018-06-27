7bf825b7be9bf453e80f124c8ae7163d2aa2bd93
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-27 17:01:55 -0500

    printing new solutions

--- Bee_Colony/ABC.py
+++ Bee_Colony/ABC.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import copy
 
 n_iterations = 5
 n_solutions = 3
@@ -17,6 +18,9 @@ min_y = -5
 max_y = 5
 
 solutions = []
+new_solutions = []
+best_solution = { "v":[ -1,-1], "func": -1, "fit": -1, 
+											"cont": -1 }
 
 def function( x ):
 	return x[0]**2 + x[1]**2
@@ -36,10 +40,12 @@ def init_solutions():
 		vect.append(y)
 		func = function( vect )
 		fit = fitness( func )
-		solutions.append( { "v":[ x, y], "func": func, "fit": fit, "cont": 0 } )
+		solutions.append( { "v":[ x, y], "func": func, "fit": fit, 
+											"cont": 0 } )
 
-def new_solutions():
-
+def get_new_solutions():
+	global new_solutions
+	new_solutions = []
 	for m in range( n_solutions ):
 		phi = random.uniform( -1, 1 )
 		j = random.randint( 0, n_dimensions-1 )
@@ -48,24 +54,57 @@ def new_solutions():
 			if i == j:
 				tmp = solutions[m]["v"][i] +  phi * ( solutions[m]["v"][i] -
 					  					solutions[k]["v"][i] )
-				solutions[m]["v"][i] = tmp
-				new_func = function( solutions[m]["v"] )
+				new_v  = [0]*n_dimensions
+				new_v[i] = tmp
+				new_v[new_v.index( 0 )] = solutions[m]["v"][new_v.index( 0 )]
+				new_func = function( new_v )
 				new_fit = fitness( new_func )
-				if new_fit > solutions[m]["fit"]:
-					solutions[m]["cont"] += 1
-				solutions[m]["fit"] = new_fit
+				new_solutions.append( { "v": new_v,"func": new_func,"fit": new_fit} )
 
+def roulette_selection():
+	rand = random.uniform( 0, 1 )
+	sum_fitness = sum([ i["fit"] for i in solutions ])
+	s = 0. 
+	for i in range( len( solutions )):
+		s += (solutions[i]["fit"] / sum_fitness) 
+		if s > rand:
+			return i
+
+def print_solutions():
+	print( "V_1\t\t\tV_2\t\t\tFunction\t\tFitness\t\t\tCounter" )
+	for i in solutions:
+		print( str(i["v"][0])+"\t"+str(i["v"][1])+"\t"+str(i["func"])+"\t"
+						+str(i["fit"])+"\t"+str(i["cont"]) )
+
+def print_new_solutions():
+	print( "V_1\t\t\tV_2\t\t\tFunction\t\tFitness" )
+	for i in new_solutions:
+		print( str(i["v"][0])+"\t"+str(i["v"][1])+"\t"+str(i["func"])+"\t"
+						+str(i["fit"]))
+
+def compare_solutions():
+	for i in range( n_solutions ):
+		if new_solutions[i]["fit"] > solutions[i]["fit"]:
+			print("asdasdasfskdjfl")
+			solutions[i]["v"] = copy.deepcopy( new_solutions[i]["v"] )
+			solutions[i]["func"] = copy.deepcopy( new_solutions[i]["func"] )
+			solutions[i]["fit"] = copy.deepcopy( new_solutions[i]["fit"] )
+		else:
+			solutions[i]["cont"] += 1 
 
 def abc():
-
+	print("Initializing population ------- ")
 	init_solutions()
-	# print( solutions )
-	new_solutions()
-	# print( solutions )
-
-
+	print_solutions( )
 	for i in range( n_iterations ):
-		print( "Iteration ++++++ ", i ) 
+		print( "Iteration ++++++ ", i )
+		get_new_solutions()
+		print_new_solutions( )
+		compare_solutions()
+		print_solutions()
+
+		a = roulette_selection()
+		print(a)
 
 if __name__=="__main__":
 	abc()
